54c443539d44a586e245cfa442262bc26f2d21b4
Simon Godden <sgodden@gmail.com>
2012-09-10 21:34:33 +0100

    Some more simple playing with mongo and node

--- nodejs/HelloWorld/routes/user.js
+++ nodejs/HelloWorld/routes/user.js
@@ -1,8 +1,23 @@
+var mongodb = require('mongodb'),
+    server = new mongodb.Server('localhost', mongodb.Connection.DEFAULT_PORT),
+    conn = new mongodb.Db('orderManagement-dev', server);
+
 
 /*
  * GET users listing.
  */
 
 exports.list = function(req, res){
+
+  conn.open(function(err, db) {
+      console.log('Opened it!');
+      db.collectionNames(function(err, colls){
+          console.log(colls);
+      });
+      db.collection('customerOrders').find().each(function(err, item){
+          console.log(item);
+      });
+  });
+
   res.send("respond with a resource");
 };
