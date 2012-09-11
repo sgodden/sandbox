59b16578c3e259c2b3dbcf06b4a9d24926cfd183
Simon Godden <sgodden@gmail.com>
2012-09-11 08:09:04 +0100

    Repeated operations now work

--- nodejs/HelloWorld/routes/user.js
+++ nodejs/HelloWorld/routes/user.js
@@ -10,14 +10,28 @@ var mongodb = require('mongodb'),
 exports.list = function(req, res){
 
   conn.open(function(err, db) {
-      console.log('Opened it!');
+      var docs = [];
+/*
       db.collectionNames(function(err, colls){
           console.log(colls);
       });
+*/
       db.collection('customerOrders').find().each(function(err, item){
-          console.log(item);
+          if (item) {
+              console.log(item);
+              docs.push(item);
+          }
+          else {
+              // we get a null item when there are no more
+              /*
+               * I think res.send is a once-only operation which commits the response,
+               * so the pattern is to build up the response and res.send it only when finished.
+               */
+              res.send(docs);
+              conn.close();
+          }
       });
   });
 
-  res.send("respond with a resource");
+  //res.send("respond with a resource");
 };
