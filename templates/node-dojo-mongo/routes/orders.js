aa60b4a18fd2ff2cf4dc9a1ef7a05d914200e91c
Simon Godden <simon@blackcatsolutions.co.uk>
2013-01-28 16:50:47 +0000

    A kind of working raw mongo and node app

---
+++ templates/node-dojo-mongo/routes/orders.js
@@ -0,0 +1,35 @@
+var repo = require('../repo/repo'),
+    CustomerOrderRepository = repo.CustomerOrderRepository;
+
+
+/*
+ * GET users listing.
+ */
+
+exports.list = function(req, res){
+
+    var repo = new CustomerOrderRepository(),
+        doList;
+
+    doList = function() {
+        repo.findAll().then(function(docs){
+            res.render('orders', {title: 'Orders', orders: docs});
+        });
+    }
+
+    repo.count().then(function(count){
+        if (count === 0) {
+            console.log('There are no orders - inserting one');
+            repo.insert({
+                orderNumber: "O000001",
+                customerReference: "CR00001"
+            })
+                .then(doList);
+        }
+        else {
+            doList();
+        }
+    });
+
+
+};
