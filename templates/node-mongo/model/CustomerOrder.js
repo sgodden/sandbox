fd026920cf00810dba920a7ca1bb9510611ecb84
Simon Godden <simon@blackcatsolutions.co.uk>
2013-02-04 21:02:05 +0000

    doco

--- templates/node-mongo/model/CustomerOrder.js
+++ templates/node-mongo/model/CustomerOrder.js
@@ -1,13 +1,23 @@
 var somePrivateFunction, CustomerOrder;
 
+/*
+ * Private function declarations.
+ */
 somePrivateFunction = function(orderNumber) {
     console.log("I am private: " + orderNumber);
 };
 
+/**
+ * @class CustomerOrder.
+ * @constructor
+ */
 CustomerOrder = function(){
     this.orderNumber = null;
 };
 
+/**
+ * @method logOrderNumber.
+ */
 CustomerOrder.prototype.logOrderNumber = function() {
     somePrivateFunction(this.orderNumber);
 };
