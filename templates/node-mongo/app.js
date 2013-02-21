6bdfe439be04f5e87b6d2e96cf3347d6ea0cda9a
Simon Godden <simon@blackcatsolutions.co.uk>
2013-02-21 08:00:08 +0000

    Switch to using dojo front end for a bit

--- templates/node-mongo/app.js
+++ templates/node-mongo/app.js
@@ -21,7 +21,7 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public-angular')));
+  app.use(express.static(path.join(__dirname, 'public-dojo')));
 });
 
 app.configure('development', function(){
