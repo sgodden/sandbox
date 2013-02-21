c53764c780fcd57cb4901ecf247a3d351a927f69
Simon Godden <simon@blackcatsolutions.co.uk>
2013-02-21 07:56:47 +0000

    Allow for different front end implementations

--- templates/node-mongo/app.js
+++ templates/node-mongo/app.js
@@ -21,7 +21,7 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, 'public-angular')));
 });
 
 app.configure('development', function(){
