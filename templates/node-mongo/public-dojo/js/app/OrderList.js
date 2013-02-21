08701ee41d2768f457dcec6038abed71b543fbf6
Simon Godden <simon@blackcatsolutions.co.uk>
2013-02-21 08:34:58 +0000

    Working dojo config

---
+++ templates/node-mongo/public-dojo/js/app/OrderList.js
@@ -0,0 +1,10 @@
+define([
+    "dojo/_base/declare",
+    "put-selector/put"
+], function(declare, put){
+    return declare("app.OrderList", [], {
+        constructor: function() {
+            console.log('werwer');
+        }
+    });
+});
