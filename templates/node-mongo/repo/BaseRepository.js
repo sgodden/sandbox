83f3a3e7b8d2cb9601d4b41efdb2e5ee1405be07
Simon Godden <simon@blackcatsolutions.co.uk>
2013-03-30 16:26:25 +0000

    Bit of refactoring to remove duplicated code.

--- templates/node-mongo/repo/BaseRepository.js
+++ templates/node-mongo/repo/BaseRepository.js
@@ -49,41 +49,48 @@ BaseRepository.prototype.execDb = function (callback) {
     return ret.promise;
 };
 
+BaseRepository.prototype.execCollection = function (callback) {
+	var self = this;
+	return this.execDb(function (db, d) {
+		db.collection(self.COLL_NAME, function (err, coll) {
+			if (err) {
+				throw new Error("Error accessing collection");
+			}
+			callback(coll, d);
+		});
+	});
+}
+
 BaseRepository.prototype.entityClass = null;
 
 BaseRepository.prototype.count = function () {
-    var self = this;
-    return this.execDb(function (db, d) {
-        db.collection(self.COLL_NAME, function (err, coll) {
-            coll.count(function (err, count) {
-                if (err) {
-                    console.log('An error occurred on count');
-                    console.log(err);
-                    throw new Error(JSON.stringify(err));
-                }
-                d.resolve(count);
-            });
-        });
+    return this.execCollection(function (coll, d) {
+		coll.count(function (err, count) {
+			if (err) {
+				console.log('An error occurred on count');
+				console.log(err);
+				throw new Error(JSON.stringify(err));
+			}
+			d.resolve(count);
+		});
     });
 };
 
 BaseRepository.prototype.findAll = function() {
     var self = this;
-    return this.execDb(function (db, d) {
+    return this.execCollection(function (coll, d) {
         var entities = [], entity;
-        db.collection(self.COLL_NAME, function (err, coll) {
-            coll.find().each(function (err, item) {
-                if (item) {
-                    entity = new self.entityClass();
-                    entity.hydrate(item);
-                    entities.push(entity);
-                }
-                else {
-                    // we get a null item when there are no more
-                    d.resolve(entities);
-                }
-            });
-        });
+		coll.find().each(function (err, item) {
+			if (item) {
+				entity = new self.entityClass();
+				entity.hydrate(item);
+				entities.push(entity);
+			}
+			else {
+				// we get a null item when there are no more
+				d.resolve(entities);
+			}
+		});
     });
 };
 
@@ -92,75 +99,64 @@ BaseRepository.prototype.findAll = function() {
  * @return {Object} a promise.
  */
 BaseRepository.prototype.insert = function (docs) {
-    var self = this;
-    return this.execDb(function (db, d) {
-        db.collection(self.COLL_NAME, function (err, coll) {
-            coll.insert(docs, {safe: true}, function (err, docs) {
-                if (err) {
-                    throw new Error(err);
-                }
-                else {
-                    d.resolve(docs);
-                }
-            });
-        });
+    return this.execCollection(function (coll, d) {
+		coll.insert(docs, {safe: true}, function (err, docs) {
+			if (err) {
+				throw new Error(err);
+			}
+			else {
+				d.resolve(docs);
+			}
+		});
     });
 };
 
 BaseRepository.prototype.findOne = function(query) {
 	var self = this;
 	query = fixId(query);
-	return this.execDb(function (db, d) {
-		db.collection(self.COLL_NAME, function (err, coll) {
-			coll.findOne(query, function (err, doc) {
-				var entity;
-				if (err) {
-					throw new Error(err);
+	return this.execCollection(function (coll, d) {
+		coll.findOne(query, function (err, doc) {
+			var entity;
+			if (err) {
+				throw new Error(err);
+			}
+			else {
+				if (doc) {
+					entity = new self.entityClass();
+					entity.hydrate(doc);
 				}
-				else {
-					if (doc) {
-						entity = new self.entityClass();
-						entity.hydrate(doc);
-					}
-					d.resolve(entity);
-				}
-			});
+				d.resolve(entity);
+			}
 		});
 	});
 };
 
 BaseRepository.prototype.update = function(query, doc) {
-	var self = this;
-	return this.execDb(function (db, d) {
-		db.collection(self.COLL_NAME, function (err, coll) {
-			coll.update(query, doc, function (err) {
-				if (err) {
-					throw new Error(err);
-				} else {
-					d.resolve();
-				}
-			});
+	return this.execCollection(function (coll, d) {
+		coll.update(query, doc, function (err) {
+			if (err) {
+				throw new Error(err);
+			} else {
+				d.resolve();
+			}
 		});
 	});
 };
 
 BaseRepository.prototype.updateById = function(object) {
-	var self = this,
-		query = {
+	var query = {
 			_id: new mongodb.ObjectID(object.id)
 		},
 		clone = lang.clone(object);
 	// we don't want to store the id twice for no purpose
 	delete clone.id;
-	return this.execDb(function (db, d) {
-		db.collection(self.COLL_NAME, function (err, coll) {
-			coll.update(query, clone, function (err) {
-				if (err) {
-					throw new Error(err);
-				} else {
-					d.resolve();
-				}
-			});
+	return this.execCollection(function (coll, d) {
+		coll.update(query, clone, function (err) {
+			if (err) {
+				throw new Error(err);
+			} else {
+				d.resolve();
+			}
 		});
 	});
 };
